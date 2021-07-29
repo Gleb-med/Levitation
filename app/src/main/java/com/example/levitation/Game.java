12ea3308f2c43e8407d27bbb3f0730446caa036e
Gleb-med <cubicscience@yandex.ru>
2021-07-29 16:06:09 +0300

    Setup

---
+++ app/src/main/java/com/example/levitation/Game.java
@@ -0,0 +1,13 @@
+package com.example.levitation;
+
+import android.content.Context;
+import android.view.Surface;
+import android.view.SurfaceView;
+import android.view.View;
+
+class Game extends SurfaceView {
+
+    public Game(Context context) {
+        super(context);
+    }
+}
