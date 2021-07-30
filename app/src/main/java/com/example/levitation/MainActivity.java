417260f86363675c8968c63e4c5f9812c76745b7
Gleb-med <cubicscience@yandex.ru>
2021-07-30 08:52:29 +0300

    Game loop

--- app/src/main/java/com/example/levitation/MainActivity.java
+++ app/src/main/java/com/example/levitation/MainActivity.java
@@ -3,13 +3,24 @@ package com.example.levitation;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //Set window to fullscreen (hide status bar)
+        Window window  = getWindow();
+        window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
+
         //setContentView(R.layout.activity_main);
+        //set content view to game, so that objects in the Game class can be rendered to the screen
         setContentView(new Game(this));
     }
 }
